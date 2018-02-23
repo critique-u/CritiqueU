bcd4f34d17c365c83abd45e7c48f160008ddb306
mikeboettner <mikeboettner@gmail.com>
2018-02-23 14:51:56 -0500

    Updates
    - addressing defect of database connections not closing causing MySql instance to go over limit
    - updated email regex

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -37,17 +37,13 @@ public class Controller extends HttpServlet {
         // TODO Auto-generated constructor stub
     }
 
-    
-    
-    
-    
-    
 	/**
 	 * @see Servlet#init(ServletConfig)
 	 */
 	public void init(ServletConfig config) throws ServletException
 	{
-		try {
+		try
+		{
 			InitialContext initContext = new InitialContext();
 			
 			Context env = (Context)initContext.lookup("java:comp/env");
@@ -55,20 +51,15 @@ public class Controller extends HttpServlet {
 			//now I can use this context to look up my data source (the mysql aws database)
 			
 			ds = (DataSource)env.lookup("jdbc/critiqueudb");
-			
-			
-		} catch (NamingException e) {
+		}
+		catch (NamingException e)
+		{
 			// TODO Auto-generated catch block
 			//e.printStackTrace();
 			throw new ServletException();
 		}
 	}
 
-	
-	
-	
-	
-	
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
@@ -77,82 +68,246 @@ public class Controller extends HttpServlet {
 		PrintWriter out = response.getWriter();
 		String action = request.getParameter("action");
 
-		if (action == null) {
+		if (action == null)
+		{
 			request.getRequestDispatcher("/home.jsp").forward(request, response);
 		}
-		else if(action.equals("login")) {
+		else if(action.equals("login"))
+		{
 			request.setAttribute("email", "");
 			request.setAttribute("password", "");
 			request.setAttribute("message", "");
 			request.getRequestDispatcher("/login.jsp").forward(request, response);
 		}
-		else if(action.equals("createaccount")) {
+		else if(action.equals("createaccount"))
+		{
 			request.setAttribute("email", "");
 			request.setAttribute("password", "");
 			request.setAttribute("repeatpassword", "");
 			request.setAttribute("message", "");
 			request.getRequestDispatcher("/createaccount.jsp").forward(request, response);
 		}
-		else {
+		else
+		{
 			out.println("unrecognized action");
 			return;
 		}
 		//getServletContext().getRequestDispatcher("/home.jsp").forward(request, response);
 	}
 
-	
-	
-	
-	
-	
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
 	{
-		//use connection
-				PrintWriter out = response.getWriter();
-				
-				String action = request.getParameter("action");
-				
-				if(action == null)
+		PrintWriter out = response.getWriter();
+		
+		String action = request.getParameter("action");
+		
+		if(action == null)
+		{
+			out.println("unrecognized action");
+			return;
+		}
+		
+		HttpSession mySession = request.getSession();
+		
+		Connection conn = null;
+		
+		try
+		{
+			conn = ds.getConnection();
+		}
+		catch (SQLException e)
+		{
+			throw new ServletException();
+		}
+		
+		Account account = new Account(conn);
+		
+		if(action.equals("dologin"))
+		{
+			String email = request.getParameter("email");
+			String password = request.getParameter("password");
+			
+			User user = new User(email, password);
+			
+			//set these request attributes. In case of login failure, they will
+				//autopopulate in the form for a retry (empty string password is intended)
+			request.setAttribute("email", email);
+			request.setAttribute("password", "");
+			
+			//if we successfully log in, forward to the loginsuccess.jsp page
+			try {
+				if(account.login(email, password)) //Account object attempts authentication and returns boolean 
 				{
-					out.println("unrecognized action");
-					return;
+					//set the User bean as a session variable
+					// get the session object
+					mySession = request.getSession();
+					
+					String emailTemp = (String) mySession.getAttribute("email");
+					
+					if(emailTemp == null)
+						emailTemp = email;
+					
+					mySession.setAttribute("email", email);
+					mySession.setAttribute("loggedin", "true");
+					
+					//close database connection
+					try
+					{
+						conn.close();
+						account.closeDBConnection();
+					}
+					catch (SQLException e)
+					{
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+					
+					request.getRequestDispatcher("/loginsuccess.jsp").forward(request, response);
 				}
-				
-				HttpSession mySession = request.getSession();
-				
-				Connection conn = null;
-				
+				else
+				{
+					//set an error message as an attribute "message"
+					request.setAttribute("message", "Error! Email address or password is incorrect.");
+					
+					//close database connection
+					try
+					{
+						conn.close();
+						account.closeDBConnection();
+					}
+					catch (SQLException e)
+					{
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+					
+					//and forward back to the login page
+					request.getRequestDispatcher("/login.jsp").forward(request, response);
+				}
+			}
+			catch (SQLException e)
+			{
+				// TODO: Do something sensible here, like forward to an error.jsp
+				e.printStackTrace();
+			}
+			finally
+			{
 				try
 				{
-					conn = ds.getConnection();
+					conn.close();
+					account.closeDBConnection();
 				}
 				catch (SQLException e)
 				{
-					throw new ServletException();
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+			
+			
+			
+			//TODO: if the users login info can be found in the db, then we go to the success page
+				// if it can't, we go back to the form.
+			//request.getRequestDispatcher("/loginsuccess.jsp").forward(request, response);
+			
+			
+		}
+		else if(action.equals("createaccount"))
+		{
+			String email = request.getParameter("email");
+			String password = request.getParameter("password");
+			String repeatPassword = request.getParameter("repeatpassword");
+			
+			request.setAttribute("email", email);
+			request.setAttribute("password", "");
+			request.setAttribute("repeatpassword", "");
+			request.setAttribute("message", "");
+			
+			if(!password.equals(repeatPassword))
+			{
+				request.setAttribute("message", "Error! Passwords do not match.");
+				
+				//close database connection
+				try
+				{
+					conn.close();
+					account.closeDBConnection();
+				}
+				catch (SQLException e)
+				{
+					// TODO Auto-generated catch block
+					e.printStackTrace();
 				}
 				
-				Account account = new Account(conn);
+				request.getRequestDispatcher("/createaccount.jsp").forward(request, response);
+			}
+			else
+			{
+				User user = new User(email, password);
 				
-				if(action.equals("dologin"))
+				if(!user.validate())
 				{
-					String email = request.getParameter("email");
-					String password = request.getParameter("password");
+					//the email or password is in the wrong format
+					request.setAttribute("message", user.getMessage());
 					
-					User user = new User(email, password);
+					//close database connection
+					try
+					{
+						conn.close();
+						account.closeDBConnection();
+					}
+					catch (SQLException e)
+					{
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
 					
-					//set these request attributes. In case of login failure, they will
-						//autopopulate in the form for a retry (empty string password is intended)
-					request.setAttribute("email", email);
-					request.setAttribute("password", "");
-					
-					//if we successfully log in, forward to the loginsuccess.jsp page
-					try {
-						if(account.login(email, password)) //Account object attempts authentication and returns boolean 
+					request.getRequestDispatcher("/createaccount.jsp").forward(request, response);
+				}
+				else
+				{
+					try
+					{
+						if(account.exists(email))
 						{
-							//set the User bean as a session variable
+							//the email already exists in the user database
+							
+							//close database connection
+							try
+							{
+								conn.close();
+								account.closeDBConnection();
+							}
+							catch (SQLException e)
+							{
+								// TODO Auto-generated catch block
+								e.printStackTrace();
+							}
+							
+							request.setAttribute("message", "Error! An account with this email address already exists.");
+							request.getRequestDispatcher("/createaccount.jsp").forward(request, response);
+						}
+						else
+						{
+							//passes all checks. Create the account
+							account.create(email, password);
+							
+							//close database connection
+							try
+							{
+								conn.close();
+								account.closeDBConnection();
+							}
+							catch (SQLException e)
+							{
+								// TODO Auto-generated catch block
+								e.printStackTrace();
+							}
+							
+							//set the user's email as a session variable
 							// get the session object
 							mySession = request.getSession();
 							
@@ -162,118 +317,77 @@ public class Controller extends HttpServlet {
 								emailTemp = email;
 							
 							mySession.setAttribute("email", email);
-							mySession.setAttribute("loggedin", "true");
-							request.getRequestDispatcher("/loginsuccess.jsp").forward(request, response);
+							request.getRequestDispatcher("/createsuccess.jsp").forward(request, response);
 						}
-						else
-						{
-							//set an error message as an attribute "message"
-							request.setAttribute("message", "Error! Email address or password is incorrect.");
-							//and forward back to the login page
-							request.getRequestDispatcher("/login.jsp").forward(request, response);
-						}
-					} catch (SQLException e) {
-						// TODO: Do something sensible here, like forward to an error.jsp
-						e.printStackTrace();
 					}
-					
-					
-					
-					//TODO: if the users login info can be found in the db, then we go to the success page
-						// if it can't, we go back to the form.
-					//request.getRequestDispatcher("/loginsuccess.jsp").forward(request, response);
-					
-					
-				}
-				else if(action.equals("createaccount"))
-				{
-					String email = request.getParameter("email");
-					String password = request.getParameter("password");
-					String repeatPassword = request.getParameter("repeatpassword");
-					
-					request.setAttribute("email", email);
-					request.setAttribute("password", "");
-					request.setAttribute("repeatpassword", "");
-					request.setAttribute("message", "");
-					
-					if(!password.equals(repeatPassword))
+					catch(SQLException e)
 					{
-						request.setAttribute("message", "Error! Passwords do not match.");
-						request.getRequestDispatcher("/createaccount.jsp").forward(request, response);
+						e.printStackTrace();
+						//request.getRequestDispatcher("/error.jsp").forward(request, response);
 					}
-					else
+					finally
 					{
-						User user = new User(email, password);
-						
-						if(!user.validate())
+						try
 						{
-							//the email or password is in the wrong format
-							request.setAttribute("message", user.getMessage());
-							request.getRequestDispatcher("/createaccount.jsp").forward(request, response);
+							conn.close();
+							account.closeDBConnection();
 						}
-						else
+						catch (SQLException e)
 						{
-							try
-							{
-								if(account.exists(email))
-								{
-									//the email already exists in the user database
-									request.setAttribute("message", "Error! An account with this email address already exists.");
-									request.getRequestDispatcher("/createaccount.jsp").forward(request, response);
-								}
-								else
-								{
-									//passes all checks. Create the account
-									account.create(email, password);
-									
-									//set the user's email as a session variable
-									// get the session object
-									mySession = request.getSession();
-									
-									String emailTemp = (String) mySession.getAttribute("email");
-									
-									if(emailTemp == null)
-										emailTemp = email;
-									
-									mySession.setAttribute("email", email);
-									request.getRequestDispatcher("/createsuccess.jsp").forward(request, response);
-								}
-							}
-							catch(SQLException e)
-							{
-								e.printStackTrace();
-								//request.getRequestDispatcher("/error.jsp").forward(request, response);
-							}
+							// TODO Auto-generated catch block
+							e.printStackTrace();
 						}
 					}
 				}
-				else if(action.equals("logout"))
-				{
-					out.println("jquery logout button worked");
-					try {
-						conn.close();
-					} catch (SQLException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
-					
-					//invalidate the session
-					request.setAttribute("email", null);
-					mySession.setAttribute("email", null);
-					mySession.invalidate();
-					request.getRequestDispatcher("/home.jsp").forward(request, response);
-				}
-				else
-				{
-					out.println("unrecognized action");
-					try {
-						conn.close();
-					} catch (SQLException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
-					return;
-				}
+			}
+		}
+		else if(action.equals("logout"))
+		{
+			//close the database connection
+			try
+			{
+				conn.close();
+				account.closeDBConnection();
+			}
+			catch (SQLException e)
+			{
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+			//invalidate the session
+			request.setAttribute("email", null);
+			mySession.setAttribute("email", null);
+			mySession.invalidate();
+			request.getRequestDispatcher("/home.jsp").forward(request, response);
+		}
+		else
+		{
+			out.println("unrecognized action");
+			try
+			{
+				conn.close();
+				account.closeDBConnection();
+			}
+			catch (SQLException e)
+			{
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			return;
+		}
+		
+		try
+		{
+			conn.close();
+			account.closeDBConnection();
+		}
+		catch (SQLException e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 	}
 
 }
